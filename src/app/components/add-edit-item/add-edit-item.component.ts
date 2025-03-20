7cd000cc6b365279a3f54fa1ee5a3af799fa89b4
Hanna Seithe <hannaseithe@googlemail.com>
2025-03-20 12:47:40 +0100

    Image Upload

--- src/app/components/add-edit-item/add-edit-item.component.ts
+++ src/app/components/add-edit-item/add-edit-item.component.ts
@@ -8,7 +8,12 @@ import {
   ViewChild,
   WritableSignal,
 } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import {
   ActivatedRoute,
   NavigationStart,
@@ -54,7 +59,15 @@ export class AddEditItemComponent {
   @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
   id: string | undefined = undefined;
   isAddMode: boolean = true;
-  form: FormGroup;
+  form: FormGroup<{
+    name: FormControl<string>;
+    description: FormControl<string>;
+    boxID: FormControl<string>;
+    tags: FormControl<Array<string>>;
+    catID: FormControl<string>;
+    picture: FormControl<string>;
+  }>;
+  pictureFile: File | null = null;
   boxes: Box[] = [];
   cats: Signal<Cat[]>;
   picture: WritableSignal<string | undefined> = signal(undefined);
@@ -67,21 +80,22 @@ export class AddEditItemComponent {
     private location: Location,
     private undo: UndoService,
     private dialog: MatDialog,
-    public nas: NasService
+    public nas: NasService,
+    private fb: NonNullableFormBuilder
   ) {
-    this.form = new FormGroup({
-      name: new FormControl(''),
-      description: new FormControl(''),
-      boxID: new FormControl(''),
-      tags: new FormControl([]),
-      catID: new FormControl(''),
-      picture: new FormControl(''),
+    this.form = this.fb.group({
+      name: '',
+      description: '',
+      boxID: '',
+      tags: new FormControl<string[]>([], { nonNullable: true }),
+      catID: '',
+      picture: '',
     });
     this.id = this.route.snapshot.params['id'];
     this.isAddMode = !this.id;
 
     effect(() => {
-      const item = this.item()
+      const item = this.item();
       if (this.nas.loggedIn() && item?.picture) {
         this.fetchThumbnail(item.picture);
       }
@@ -102,6 +116,9 @@ export class AddEditItemComponent {
       let item = this.item();
       if (item) {
         this.form.patchValue(item);
+        if (item.picture && this.nas.loggedIn()) {
+          this.fetchThumbnail(item.picture);
+        }
       }
     }
 
@@ -111,24 +128,23 @@ export class AddEditItemComponent {
     }
   }
 
-  private fetchThumbnail(fileName:string){
-    this.nas.fetchThumbnail(fileName,100).subscribe({
+  private fetchThumbnail(fileName: string) {
+    this.nas.fetchThumbnail(fileName, 100).subscribe({
       next: (blob) => {
-        this.convertBlobToBase64(blob).then(base64 => {
-          this.picture.set(base64); 
+        this.convertBlobToBase64(blob).then((base64) => {
+          this.picture.set(base64);
         });
       },
-      error: (error) => console.error(error)
-      
+      error: (error) => console.error(error),
     });
   }
 
-  private convertBlobToBase64(blob:Blob):Promise<string> {
+  private convertBlobToBase64(blob: Blob): Promise<string> {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.readAsDataURL(blob);
-      reader.onloadend = () => resolve(reader.result as string); 
-      reader.onerror = error => reject(error);
+      reader.onloadend = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
     });
   }
 
@@ -156,13 +172,17 @@ export class AddEditItemComponent {
 
   remove(tag: string) {
     let tags = this.form.get('tags')?.value;
-    const index = tags.indexOf(tag);
-    if (index < 0) {
-      return tags;
-    }
+    if (tags) {
+      const index = tags.indexOf(tag);
+      if (index < 0) {
+        return tags;
+      }
 
-    tags.splice(index, 1);
-    return [...tags];
+      tags.splice(index, 1);
+      this.form.markAsDirty();
+      return [...tags];
+    }
+    return undefined;
   }
 
   add(event: MatChipInputEvent) {
@@ -184,15 +204,41 @@ export class AddEditItemComponent {
 
   onSubmit() {
     let formItem = this.form.value;
-    formItem.id = this.id ? this.id : undefined;
-    let { item, resetFn } = this.data.addUpdateItem(formItem);
+    if (this.pictureFile && this.nas.loggedIn()) {
+      this.nas.uploadFile(this.pictureFile, true).subscribe({
+        next: (response: any) => {
+          if (response.status === 1) {
+            console.log('upload successful');
+            this.saveForm(formItem, response.name);
+          } else {
+            console.error('Upload Failed');
+            this.saveForm(formItem);
+          }
+        },
+        error: (error) => {
+          console.error(error);
+          this.saveForm(formItem);
+        },
+      });
+    } else {
+      this.saveForm(formItem);
+    }
+  }
+
+  private saveForm(formItem: any, pictureName?: string) {
+    let saveItem =
+      pictureName != undefined
+        ? { ...formItem, picture: pictureName }
+        : formItem;
+    saveItem.id = this.id ? this.id : undefined;
+    let { item, resetFn } = this.data.addUpdateItem(saveItem);
     if (resetFn) {
       this.undo.push(resetFn);
     }
     if (this.id) {
       this.router.navigateByUrl('/item/' + item()?.id);
     } else {
-      this.router.navigateByUrl('/box/' + formItem.boxID);
+      this.router.navigateByUrl('/box/' + item()?.boxID);
     }
   }
 
@@ -200,24 +246,8 @@ export class AddEditItemComponent {
     this.location.back();
   }
 
-  async onFileSelect(event: any): Promise<void> {
-    const file = event.target.files[0];
-    console.log('File selected');
-
-    if (file) {
-      const fileName = this.nas.uploadFile(file, true).subscribe({
-        next: (response: any) => {
-          if (response.status === 1) {
-            console.log('upload successful');
-            this.form.patchValue({ picture: response.name });
-          } else {
-            console.error('Upload Failed');
-          }
-        },
-        error: (error) => {
-          console.error(error);
-        },
-      });
-    }
+  onFileSelect(event: any): void {
+    this.pictureFile = event.target.files[0];
+    this.form.markAsDirty();
   }
 }
