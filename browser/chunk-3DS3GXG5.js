aa5df300de4752493671671b08b116eb6dd9c01a
Hanna Seithe <hannaseithe@googlemail.com>
2025-05-09 11:39:35 +0200

    Auto-generated commit

---
+++ browser/chunk-3DS3GXG5.js
@@ -0,0 +1,1 @@
+import"./chunk-EQDQRRRY.js";var a={nasFolderPath:"/Multimedia",nasUserName:"admin1",nasPassword:"Meisenweg16"};export{a as nasConfig};
---
+++ browser/chunk-4IQU2XEI.js
@@ -0,0 +1,1 @@
+import"./chunk-EQDQRRRY.js";var t={production:!0,apiUrl:"http://192.168.178.28:8080"};export{t as environment};
